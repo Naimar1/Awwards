912fadf41af40242c64c562aa54f038074a5187a
naimar1 <naiglyme1@gmail.com>
2019-04-05 10:28:54 +0200

    create new profile in views function

--- awwardapps/views.py
+++ awwardapps/views.py
@@ -45,3 +45,19 @@ def rating(request,id):
     else:
         return redirect('index') 
 
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    if request.method=='POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.username = current_user
+
+            profile.save()
+        return redirect('Index')
+    else:
+        form=ProfileForm()
+
+    return render(request,'create_profile.html',{"form":form})
+
